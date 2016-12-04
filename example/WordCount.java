815a672271cae05e3b0a422045247fb7bcd373df
Yumin Xia <me@yumin-xia.com>
2016-12-04 11:34:35 -0800

    use naive way to do it..

--- example/WordCount.java
+++ example/WordCount.java
@@ -12,7 +12,155 @@ import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
-import Paillier.*;
+import java.io.FileWriter;
+
+// Paillier class
+
+import java.math.BigInteger;
+import java.util.*;
+
+class Paillier
+{
+    public static BigInteger powpow(BigInteger base, BigInteger exponent, BigInteger mods)
+    {
+        BigInteger result = base.modPow(exponent, mods);
+
+        return result;
+    }
+
+    public static BigInteger invMod(BigInteger a, BigInteger p) throws Exception
+    {
+        BigInteger invResult = a.modInverse(p);
+        return invResult;
+    }
+
+    public static BigInteger modPow(BigInteger base, BigInteger exp, BigInteger mod)
+    {
+        return base.modPow(exp, mod);
+    }
+
+    public static KeyPair generateKeyPair(long bits) {
+        if (bits < 8) {
+            bits = 8;
+        }
+        try {
+            BigInteger p = Primes.generatePrime(bits / 2);
+            BigInteger q = Primes.generatePrime(bits / 2);
+            BigInteger n = p.multiply(q);
+
+            PrivateKey priv = new PrivateKey(p, q, n);
+            PublicKey pub = new PublicKey(n);
+            KeyPair kp = new KeyPair(priv, pub);
+            return kp;
+        }
+        catch (Throwable e) {
+            return generateKeyPair(bits);
+        }
+    }
+
+    public static BigInteger encrypt(PublicKey pub, long plain)
+    {
+        BigInteger r;
+        double pubNlog = Math.log(pub.n.doubleValue());
+        double log2 = Math.log(2);
+        double rounded = Math.round(pubNlog / log2);
+        while(true) {
+            r = Primes.generatePrime((long)rounded);
+            if (r.compareTo(BigInteger.ZERO) == 1 && r.compareTo(pub.n) == -1) {
+                break;
+            }
+        }
+
+        BigInteger x = powpow(r, pub.n, pub.nSq);
+
+        BigInteger bigIntPlain = BigInteger.valueOf(plain);
+
+        BigInteger cipher = (powpow(pub.g, bigIntPlain, pub.nSq).multiply(x)).mod(pub.nSq);
+
+        return cipher;
+    }
+
+    public static BigInteger eAdd(PublicKey pub,BigInteger a, BigInteger b)
+    {
+        return a.multiply(b).mod(pub.nSq);
+    }
+
+    public static BigInteger eAddConst(PublicKey pub, BigInteger a,long n)
+    {
+        BigInteger bigIntN = BigInteger.valueOf(n);
+        return a.multiply(modPow(pub.g, bigIntN, pub.nSq)).mod(pub.nSq);
+    }
+
+    public static BigInteger eMulConst(PublicKey pub, BigInteger a,long n)
+    {
+        return modPow(a, BigInteger.valueOf(n), pub.nSq);
+    }
+
+    public static long decrypt(PrivateKey priv, PublicKey pub, BigInteger cipher)
+    {
+        BigInteger x = powpow(cipher, priv.l, pub.nSq).subtract(BigInteger.ONE);
+        BigInteger plain = x.divide(pub.n).multiply(priv.m).mod(pub.n);
+
+        return plain.longValue();
+    }
+
+    public static PublicKey genPublicKey(BigInteger n)
+    {
+        return new PublicKey(n);
+    }
+
+    static public class PrivateKey {
+        PrivateKey(BigInteger p, BigInteger q, BigInteger n) throws Exception {
+
+            l = (p.subtract(BigInteger.ONE)).multiply(q.subtract(BigInteger.ONE));
+            m = Paillier.invMod(l, n);
+        }
+        public BigInteger l;
+        public BigInteger m;
+    }
+
+    static public class PublicKey {
+        PublicKey(BigInteger nx) {
+            n = nx;
+            nSq = nx.multiply(nx);
+            g = nx.add(BigInteger.ONE);
+        }
+
+        public BigInteger n;
+        public BigInteger nSq;
+        public BigInteger g;
+    }
+
+    static public class KeyPair {
+        KeyPair(PrivateKey privateKey, PublicKey publicKey)
+        {
+            priv = privateKey;
+            pub = publicKey;
+        }
+        public PrivateKey priv;
+        public PublicKey pub;
+    }
+
+}
+
+class Primes {
+
+    static public BigInteger generatePrime(long nBit)
+    {
+        if (nBit < 40) {
+            nBit = 40;
+        }
+        return BigInteger.probablePrime((int)nBit, r);
+    }
+
+    static Random r = new Random();
+}
+
+
+// Paillier class end
+
+
+
 
 public class WordCount {
 
@@ -68,6 +216,16 @@ public class WordCount {
         // only the decrypt program got priv_key
         Paillier.KeyPair kp = Paillier.generateKeyPair(8);
         BigInteger pub_n = kp.pub.n;
+
+        BigInteger priv_l = kp.priv.l;
+        BigInteger priv_m = kp.priv.m;
+
+        FileWriter fw = new FileWriter("privateKey");
+        fw.write(priv_l.toString());
+        fw.write("\n");
+        fw.write(priv_m.toString());
+        fw.write("\n");
+        fw.close();
 
 
         Configuration conf = new Configuration();
--- example/compile.sh
+++ example/compile.sh
@@ -1,8 +1,10 @@
 
-export HADOOP_CLASSPATH=${JAVA_HOME}/lib/tools.jar:/home/stumble/learn/UCLA/CS211/hehe/build/Paillier.jar
+# export HADOOP_CLASSPATH=${JAVA_HOME}/lib/tools.jar:/home/stumble/learn/UCLA/CS211/hehe/build/Paillier.jar
+
+export HADOOP_CLASSPATH=${JAVA_HOME}/lib/tools.jar
 
 /home/stumble/learn/UCLA/CS211/hadoop/hd/bin/hadoop com.sun.tools.javac.Main WordCount.java
 
-jar cf wc.jar WordCount*.class
+jar cf wc.jar WordCount*.class Paillier*.class Primes*.class
 
 rm *.class

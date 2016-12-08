163c3355ec3bcbb454a80819e1e60766c3c87006
Yumin Xia <me@yumin-xia.com>
2016-12-08 15:48:42 -0800

    retirve changes on dwc scripts

--- dwc/compile.sh
+++ dwc/compile.sh
@@ -1,7 +1,8 @@
+# export HADOOP_CLASSPATH=${JAVA_HOME}/lib/tools.jar:/home/stumble/learn/UCLA/CS211/hehe/build/Paillier.jar
 
 export HADOOP_CLASSPATH=${JAVA_HOME}/lib/tools.jar
 
-hadoop com.sun.tools.javac.Main WordCount2.java
+/home/stumble/learn/UCLA/CS211/hadoop/hd/bin/hadoop com.sun.tools.javac.Main WordCount2.java
 
 jar cf wc.jar *
 
--- dwc/run.sh
+++ dwc/run.sh
@@ -1,7 +1,9 @@
 
-hd="/usr/local/hadoop"
+hd="/home/stumble/learn/UCLA/CS211/hadoop/hd"
 
 export HADOOP_HOME=${hd}
+export JAVA_HOME=/usr/lib/jvm/java-8-oracle
+# export PATH=$PATH:$HADOOP_HOME/bin;$HADOOP_HOME/sbin
 export HADOOP_CLASSPATH=${JAVA_HOME}/lib/tools.jar
 
 
